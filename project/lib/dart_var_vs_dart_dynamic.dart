814c37eb1b22414bea76d240c90d1e8b0dd3fbe7
SharmaAshish7251 <sharmaashish7251@gmail.com>
2024-11-03 20:30:07 +0530

    Dart var vs Dart dynamics

---
+++ project/lib/dart_var_vs_dart_dynamic.dart
@@ -0,0 +1,23 @@
+
+
+void main(){
+
+
+  var section;
+
+  section = 3; // Integer
+
+  section= 'Linux'; // String
+  section = false;  // boolean
+
+  print(section);
+
+
+  var rollno=8;
+
+  rollno=3;
+
+  // rollno= 'hi'; // if you initialize value in variable at declaration it will assue that all upcoming values will be same.u can't store other data types. except integer.
+
+
+}
