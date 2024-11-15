408bbd63f0dafaa3b0578c3347c64df4457ac011
SharmaAshish7251 <sharmaashish7251@gmail.com>
2024-11-15 14:24:28 +0530

    Loop and Do while loop

---
+++ project/lib/Dart_Loops_15_11.dart
@@ -0,0 +1,27 @@
+// Dart Loops
+
+void main(){
+
+  // Basic Loop
+  for (int a =1; a<=10; a++){
+    print(a);
+  }
+
+  // Do while Loop
+  for (int a =1; a<=10; a++){
+    print("Hello ");
+  }
+
+  int no=100;
+
+  do{
+
+  } while(no<50);{
+    print('Number value is : $no');
+    // no++; all methods we can use
+    // no = no+1;
+    // no+=1;
+
+  }
+
+}
