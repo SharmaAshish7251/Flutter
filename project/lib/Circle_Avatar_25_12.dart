40b30973fc2e4519471e7cc30184b17ce6c3e778
SharmaAshish7251 <sharmaashish7251@gmail.com>
2024-12-25 15:36:52 +0530

    Avatar

---
+++ project/lib/Circle_Avatar_25_12.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Circle Avatar',
+      theme: ThemeData(
+        primaryColor: Colors.pink,
+      ),
+      home: const MyHomePage(title: 'Circle Avatar'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+
+        var arrNames=['Akash','Raman','Akash','Raman','Vikash', 'Ravan','Akash','Raman','Vikash', 'Ravan','Vikash', 'Ravan'];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+
+      body:Center(
+        child: CircleAvatar(
+          child:
+          Text("data", style: TextStyle(
+            color: Colors.white,
+          ),),
+          backgroundColor: Colors.purpleAccent,
+          backgroundImage: AssetImage('assets/images/card.webp'),
+
+        ),
+      )
+
+    );
+
+  }
+}
